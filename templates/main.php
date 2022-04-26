3b812b70eb90332bc42a8f5500da948ec858918f
kiipod <iamkir90@hotmail.com>
2022-04-27 00:47:19 +0300

    Добавляю ссылку с параметром запроса на лот

--- templates/main.php
+++ templates/main.php
@@ -21,7 +21,7 @@
                 </div>
                 <div class="lot__info">
                     <span class="lot__category"><?= htmlspecialchars($lot['cat_name']); ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($lot['lot_name']); ?></a></h3>
+                    <h3 class="lot__title"><a class="text-link" href="/lot.php?id=<?= htmlspecialchars($lot['category_id']); ?>"><?= htmlspecialchars($lot['lot_name']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
