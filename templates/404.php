9ab31d28b150618c2df4e84b6f6c8cc2c49848da
kiipod <iamkir90@hotmail.com>
2022-04-27 01:51:02 +0300

    Поправил вывод категорий

--- templates/404.php
+++ templates/404.php
@@ -2,7 +2,7 @@
     <ul class="nav__list container">
     <?php foreach ($categories as $category): ?>
         <li class="nav__item">
-            <a href="all-lots.html">Доски и лыжи</a>
+            <a href="all-lots.html"><?= $category['name']; ?></a>
         </li>
     <?php endforeach; ?>
     </ul>
