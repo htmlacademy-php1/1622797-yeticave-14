c810ad9c54e2c0ed1ab6778c4d19833723519f44
kiipod <iamkir90@hotmail.com>
2022-04-27 01:50:30 +0300

    Добавил переход на 404

--- lot.php
+++ lot.php
@@ -5,12 +5,14 @@ require_once 'data.php';
 
 $lot_id = $_GET['id'];
 
-if(isset($_GET['id'])) {
-        header("Location:/404.php");
-    }
+if (!isset($_GET['id'])) {
+    header("Location:/404.php");
+}
+if (empty($_GET['id'])) {
+    header("Location:/404.php");
+}
 
 $sqlCat = get_categories($link);
-
 $lot = get_lot_id($link, $lot_id);
 
 $content = include_template('lot.php', ['categories' => $sqlCat, 'lot' => $lot]);
