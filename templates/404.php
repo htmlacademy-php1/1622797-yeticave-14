1eb31a161ae2523639cdbf1bd42e28c91152c265
kiipod <iamkir90@hotmail.com>
2022-05-07 19:23:03 +0300

    Делает правки в 404.php

--- templates/404.php
+++ templates/404.php
@@ -1,13 +1,15 @@
-<nav class="nav">
-    <ul class="nav__list container">
-    <?php foreach ($categories as $category): ?>
-        <li class="nav__item">
-            <a href="all-lots.html"><?= $category['name']; ?></a>
-        </li>
-    <?php endforeach; ?>
-    </ul>
-</nav>
-<section class="lot-item container">
-    <h2>404 Страница не найдена</h2>
-    <p>Данной страницы не существует на сайте.</p>
-</section>
+<main>
+    <nav class="nav">
+        <ul class="nav__list container">
+            <?php foreach ($categories as $category) : ?>
+                <li class="nav__item">
+                    <a href="all-lots.html"><?= $category['name']; ?></a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
+    <section class="lot-item container">
+        <h2>404 Страница не найдена</h2>
+        <p>Данной страницы не существует на сайте.</p>
+    </section>
+</main>
